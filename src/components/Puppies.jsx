3ea649f7cc6de210531c80ffd22a6956d50a710d
bjuliehockey30 <juliehockey30@gmail.com>
2022-07-05 17:05:03 -0600

    add puppies component

--- src/components/Puppies.jsx
+++ src/components/Puppies.jsx
@@ -1,5 +1,39 @@
+import { Link } from 'react-router-dom'
+
 const Puppies = () => {
-  return <div>Here is the puppies component!</div>
+  const importAll = (r) => {
+    return r.keys().map(r)
+  }
+
+  const listOfImages = importAll(
+    require.context('../assets/lastLitterPuppies', false, /\.(png|jpe?g|svg)$/)
+  )
+
+  return (
+    <div className="pb-16 flex flex-col items-center">
+      <p className="text-4xl text-darkPurple font-black text-center">
+        The next litter will be born between July 16 - July 23
+      </p>
+      <p className="text-2xl text-darkPurple font-black text-center">
+        Contact us today if you are interested in adopting a pup from this
+        litter!{' '}
+      </p>
+      <Link
+        className="bg-lightPurple hover:bg-darkPurple text-white mt-4 text-xl font-extrabold py-4 px-6 rounded-md"
+        to="/contact"
+      >
+        CONTACT US
+      </Link>
+      <p className="text-4xl text-darkPurple font-black text-center mt-16">
+        Our last litter born on June 15th, 2021
+      </p>
+      <div className="mx-8 mt-0 mb-12 flex flex-wrap">
+        {listOfImages.map((image, index) => (
+          <img className="max-h-96 m-2" key={index} src={image}></img>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Puppies
