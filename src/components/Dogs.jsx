f1cbe8ca392276c4480324080d9ff063fc7b97c4
bjuliehockey30 <juliehockey30@gmail.com>
2022-07-05 16:31:03 -0600

    add dogs component

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -1,5 +1,28 @@
+import Lulu1 from '../assets/Lulu1.jpg'
+import Lulu2 from '../assets/Lulu2.jpg'
+
 const Dogs = () => {
-  return <div>Here is the dogs component!</div>
+  return (
+    <div className="pb-16">
+      <p className="text-4xl text-darkPurple font-black text-center">
+        Meet the PAWrents
+      </p>
+      <div className="flex w-full mt-12">
+        <div className="w-1/2 text-center">
+          <p className="text-xl text-lightPurple font-black">Mom</p>
+          <p className="text-3xl text-darkPurple font-black">LULU</p>
+          <div className="flex items-center justify-center">
+            <img className="mt-8 mr-2 max-h-96" src={Lulu1} />
+            <img className="mt-8 ml-2 max-h-96" src={Lulu2} />
+          </div>
+        </div>
+        <div className="w-1/2 text-center">
+          <p className="text-xl text-lightPurple font-black">Dad</p>
+          <p className="text-3xl text-darkPurple font-black">LUMI</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dogs
--- src/components/Pupdate.jsx
+++ src/components/Pupdate.jsx
@@ -1,8 +1,8 @@
 const Pupdate = () => {
   return (
     <div className="bg-darkPurple fixed z-20 w-full flex justify-center items-center mt-32 text-white py-2 font-black">
-      PUPDATE! Our next litter will be arriving early September. Contact us
-      today if you are interested.
+      PUPDATE! Our next litter will be arriving mid-end July. Contact us today
+      if you are interested in adopting.
     </div>
   )
 }
