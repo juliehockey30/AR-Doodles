51afc2bcb59ef994f4c82e6c5c8029df1ac0ef52
bjuliehockey30 <juliehockey30@gmail.com>
2022-07-06 14:03:35 -0600

    fix lulus name spelling

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -19,7 +19,7 @@ const FAQ = () => {
         <Question text="What's a doodle?" />
         <Answer text='A "doodle" often refers to a breed that is a cross between a poodle and another breed. Lulu, the pups mom, is a "Goldendoodle" because her mom was a Golden Retriever and her Dad was a poodle.' />
         <Question text="Are your dogs hypoallergenic?" />
-        <Answer text="Yes! Because of the poodle bloodline in them, they are considered hypoallergenic. Since their dad Lumi is a pure bread standard poodle and their mom Luna also is 50% poodle, the pups are even more hypoallergenic than most doodles." />
+        <Answer text="Yes! Because of the poodle bloodline in them, they are considered hypoallergenic. Since their dad Lumi is a pure bread standard poodle and their mom Lulu also is 50% poodle, the pups are even more hypoallergenic than most doodles." />
         <Question text="How much does a puppy cost?" />
         <Answer text="The puppies cost $1500 and to reserve ahead of time, you must put down a $500 deposit. The deposit is only refundable if there are not enough puppies in the litter for everyone with a reservation. Puppies are adopted on a first come first serve basis. Unfortunately we cannot refund your deposit if there are still puppies available but the specific color or gender you would like is not." />
         <Question text="When can I pick up my puppy?" />
